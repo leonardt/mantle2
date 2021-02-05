6a41439ed5edc42c081da7eca6a51f7b06f54ca3
Lenny Truong <lenny@stanford.edu>
2021-02-05 14:31:03 -0800

    Add minimal working example

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -10,19 +10,21 @@ class QueueModel:
     @pysv.sv()
     def __init__(self, num_entries):
         # TODO: How can we encode the type information as a parameter?
-        self.num_entries = num_entires
+        self.num_entries = num_entries
         self._entries = []
 
     @pysv.sv(value=pysv.DataType.UInt)
     def enq(self, value):
         if len(self._entries) > self.num_entries:
             raise Exception("Queue is full")
+        print(f"Enq {value}")
         self._entries.append(value)
 
     @pysv.sv(return_type=pysv.DataType.UInt)
     def deq(self):
         if len(self._entries) == 0:
             raise Exception("Queue is empty")
+        print(f"Deq {self._entries[0]}")
         return self._entries.pop(0)
 
 
@@ -56,11 +58,20 @@ def test_queue_model():
         enq_data = BitVector.random(32)
         tester.circuit.enq.data = enq_data
         tester.circuit.enq.valid = enq_valid
-        if enq_valid:
-            tester.make_call(model.enq, enq_data)
+        if_tester = tester._if(
+            tester.circuit.enq.valid & tester.circuit.enq.ready)
+        if_tester.make_call_stmt(model.enq, enq_data)
+        tester.advance_cycle()
+        tester.circuit.enq.valid = 0
 
         deq_ready = Bit.random()
         tester.circuit.deq.ready = deq_ready
-        if_tester = tester._if(tester.circuit.deq.valid)
-        if_tester.circuit.deq.data.expect(tester.make_call(model.deq))
-    tester.compile_and_run("verilator", use_pysv=True)
+        if_tester = tester._if(
+            tester.circuit.deq.valid & tester.circuit.deq.ready)
+        var = if_tester.Var("deq_data", BitVector[32])
+        if_tester.poke(var, if_tester.make_call(model.deq))
+        # TODO: Expect calls function twice (second time for erorr message)
+        if_tester.circuit.deq.data.expect(var)
+        tester.advance_cycle()
+        tester.circuit.deq.ready = 0
+    tester.compile_and_run("verilator", use_pysv=True, disp_type="realtime")
