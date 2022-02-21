30c491ced366383094fb66a0edd63a0fcb362ae5
Lenny Truong <leonardtruong@fb.com>
2022-02-21 11:15:10 -0800

    Release v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mantle2",
-    version="0.0.3",
+    version="0.0.4",
     author="Lenny Truong",
     author_email="lenny@cs.stanford.edu",
     description="magma standard library (version 2)",
